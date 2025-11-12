8938c59a4a4ab50a8441bb7dd16e3dc92f4eb287
Connor Rakov <155401101+connor-r78@users.noreply.github.com>
2025-11-12 08:00:47 -0500

    Eliminate Magic Numbers

--- in_game/setup/countries/check.c
+++ in_game/setup/countries/check.c
@@ -2,48 +2,62 @@
 #include <stdio.h>
 #include <string.h>
 
+#define ARGUMENTS 2
+#define TAG_ARG 1
+
+#define SUCCESS 0
+#define ERR_INCORRECT_USAGE 1
+#define ERR_DIR_NOT_FOUND 2
+
+#define FALSE 0
+#define TRUE 1
+	
+#define DEFAULT_BUFFER 100
+#define MUTATION_LENGTH 2
+#define TAG_LENGTH 3
+
 int findTag(char *tag, char *countryfileName) 
 {
-	char mutatedTag[5];
-  snprintf(mutatedTag, sizeof(mutatedTag), "%s =", tag);
+	char mutatedTag[TAG_LENGTH + MUTATION_LENGTH];
+    snprintf(mutatedTag, sizeof(mutatedTag), "%s =", tag);
 
 	FILE *countryfile;
 	countryfile = fopen(countryfileName, "r");
 
-	char readString[100];
-	while ( fgets(readString, 100, countryfile) ) {
-		if ( strstr(readString, mutatedTag) ) return 1;
+	char readString[DEFAULT_BUFFER];
+	while ( fgets(readString, DEFAULT_BUFFER, countryfile) ) {
+		if ( strstr(readString, mutatedTag) ) return TRUE;
 	}
 
 	fclose(countryfile);
 
-	return 0;
+	return FALSE;
 }
 
 int main(int argc, char *argv[])
 {
-  if ( argc == 2 && strlen(argv[1]) == 3 ) {
+  if ( argc == ARGUMENTS && strlen(argv[TAG_ARG]) == TAG_LENGTH ) {
 		DIR *countries;
 		struct dirent *dir;
 
-		int found = 0;
+		int found = FALSE;
 
 		countries = opendir("./");
-		if (!countries) return 2;
+		if (!countries) return ERR_DIR_NOT_FOUND;
 
 		while ( (dir = readdir(countries)) ) {
-			if ( strstr(dir->d_name, ".txt") && findTag(argv[1], dir->d_name) ) {
-				printf("%s is already in use.\n", argv[1]);
-				found = 1;
+			if ( strstr(dir->d_name, ".txt") && findTag(argv[TAG_ARG], dir->d_name) ) {
+				printf("%s is already in use.\n", argv[TAG_ARG]);
+				found = TRUE;
 			}
 		}
 
 		closedir(countries);
-		if(!found) printf("%s is safe to use.\n", argv[1]);
-		return 0;
+		if(!found) printf("%s is safe to use.\n", argv[TAG_ARG]);
+		return SUCCESS;
   }
   else {
 		printf("Usage: ./check TAG\n");
-		return 1;
+		return ERR_INCORRECT_USAGE;
   }
 }
